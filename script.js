6d1f8ac88dfb68c79849ff483e3d2f75103c9c7e
h4sitha <hasithamania123@gmail.com>
2023-12-13 11:07:42 +0530

    Commit initial files

---
+++ script.js
@@ -0,0 +1,48 @@
+let scoreCard = document.getElementById("score")
+
+// console.log(scoreCard)
+
+function addOne(){
+    scoreCard.textContent = +scoreCard.textContent + 1
+    // console.log("Add one clicked")
+}
+
+function addTwo(){
+    scoreCard.textContent = +scoreCard.textContent + 2
+    // console.log("Add two clicked")
+}
+
+function addThree(){
+    scoreCard.textContent = +scoreCard.textContent + 3
+    // console.log("Add three clicked")
+}
+
+function clearContent(){
+    scoreCard.textContent = 0
+    // console.log("Reset button clicked")
+}
+
+
+let scoreCard2 = document.getElementById("score2")
+
+// console.log(scoreCard2)
+
+function addOne2(){
+    scoreCard2.textContent = +scoreCard2.textContent + 1
+    // console.log("Add one clicked")
+}
+
+function addTwo2(){
+    scoreCard2.textContent = +scoreCard2.textContent + 2
+    // console.log("Add two clicked")
+}
+
+function addThree2(){
+    scoreCard2.textContent = +scoreCard2.textContent + 3
+    // console.log("Add three clicked")
+}
+
+function clearContent2(){
+    scoreCard2.textContent = 0
+    // console.log("Reset button clicked")
+}
